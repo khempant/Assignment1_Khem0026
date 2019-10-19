7aa85abe5523342b672345d39b0d56714c807c41
Khem Pant <khem0026@edu.easj.dk>
2019-10-19 23:36:47 +0200

    AddedTestProject

---
+++ BooksAttributeTests/BookTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BooksAttributeTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void BookTest1()
+        {
+        }
+    }
+}
