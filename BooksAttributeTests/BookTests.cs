945691b18e5b3c83a7dc74207650a76ddc549639
Khem Pant <khem0026@edu.easj.dk>
2019-10-19 23:39:13 +0200

    AttributesTested

--- BooksAttributeTests/BookTests.cs
+++ BooksAttributeTests/BookTests.cs
@@ -1,13 +1,31 @@
+using BooksLibraryProject1;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace BooksAttributeTests
 {
     [TestClass]
-    public class UnitTest1
+    public class BookTest1
     {
         [TestMethod]
-        public void BookTest1()
+       
+        [ExpectedException(typeof(ArgumentException))]
+        public void test_Title()
         {
+            Book obj = new Book("Ne", "newAuthor", 94, "0123456789123");
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void test_PageNumber()
+        {
+            Book obj = new Book("NewBook", "newAuthor", 64, "0123456789123");
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void test_Isbn()
+        {
+            Book obj = new Book("NewBook", "newAuthor", 11, "012345678912");
         }
     }
-}
+    }
+
